c0b0c9c87ccac677a2f59fe854527404587bd4e5
Nobel Noberts <117828849+Noberts9@users.noreply.github.com>
2024-02-15 10:34:37 +0300

    Delete #program to check if eliglbe to vote.py

--- #program to check if eliglbe to vote.py
+++
@@ -1,8 +0,0 @@
-#program to check if eliglbe to vote
-age = int(input("Enter your age:"))
-nationality =float(input("Enter your nationality:"))
-
-if age >=18 and nationality => "Kenyan,Ugandan,Tanzanian:" 
- print("Eligible to vote")
-else:
- print("Not Eligible to vote")
